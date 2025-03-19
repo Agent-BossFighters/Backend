c745004b874e18958fbd952ade5a06f934fbad8f
AlexandreTedesco <alexandre.tedesco75@gmail.com>
2025-03-19 14:59:19 +0100

    fix editing profil

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -23,7 +23,26 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def update
-    if current_user.update(user_params)
+    # Vérifier si le mot de passe actuel est fourni
+    unless params[:user][:current_password].present?
+      return render json: { error: "Le mot de passe actuel est requis" }, status: :unprocessable_entity
+    end
+
+    # Vérifier si le mot de passe est correct
+    unless current_user.valid_password?(params[:user][:current_password])
+      return render json: { error: "Le mot de passe actuel est incorrect" }, status: :unauthorized
+    end
+    
+    # Créer les paramètres de mise à jour
+    update_params = user_params
+    
+    # Si un nouveau mot de passe est fourni, l'ajouter aux paramètres
+    if params[:user][:password].present?
+      update_params = update_params.merge(password: params[:user][:password])
+    end
+    
+    # Mettre à jour l'utilisateur
+    if current_user.update(update_params)
       render json: current_user
     else
       render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
@@ -71,9 +90,8 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def user_params
-    params.require(:user).permit(
-      :username, :isPremium, :level, :experience,
-      :assetType, :asset, :slotUnlockedId, :maxRarity
-    )
+    # Permettre username et autres attributs si nécessaire, mais pas current_password
+    params.require(:user).permit(:username, :isPremium, :level, :experience,
+    :assetType, :asset, :slotUnlockedId, :maxRarity)
   end
 end
